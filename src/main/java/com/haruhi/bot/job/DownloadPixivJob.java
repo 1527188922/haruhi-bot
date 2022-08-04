9d97f5fd7d0fac58de58b2cd6cdbd28355cd8b6b
刘昉 <1527188922@qq.com>
2022-08-04 08:45:20 +0800

    3个线程专门下载非r18 pixiv

--- src/main/java/com/haruhi/bot/job/DownloadPixivJob.java
+++ src/main/java/com/haruhi/bot/job/DownloadPixivJob.java
@@ -47,7 +47,7 @@ public class DownloadPixivJob extends AbstractJob {
         if(param == null){
             param = new HashMap<>();
             param.put("num",20);
-            param.put("r18",2);
+            param.put("r18",0);
         }
         for (int i = 0; i < 2; i++) {
             ThreadPoolFactory.getDownloadThreadPool().execute(new DownloadPixivJob.downloadTask(pixivService,param));
