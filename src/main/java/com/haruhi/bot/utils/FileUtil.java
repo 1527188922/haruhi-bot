b8a26760563dbb51f1a926ad0936baf69ce7a4c4
刘昉 <1527188922@qq.com>
2022-08-12 17:58:12 +0800

    文件工具类添工具方法

--- src/main/java/com/haruhi/bot/utils/FileUtil.java
+++ src/main/java/com/haruhi/bot/utils/FileUtil.java
@@ -3,6 +3,9 @@ package com.haruhi.bot.utils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 public class FileUtil {
     private FileUtil(){}
@@ -14,5 +17,40 @@ public class FileUtil {
         if(file.exists()){
             file.delete();
         }
+    }
+    public static File[] getAllFileList(String path){
+        return getAllFileList(new File(path));
+    }
+    public static List<File> getFileList(String path){
+        return getFileList(new File(path));
+    }
+    public static List<File> getDirectoryList(String path){
+        return getDirectoryList(new File(path));
+    }
+    public static List<File> getDirectoryList(File file){
+        ArrayList<File> res = new ArrayList<>();
+        File[] allFileList = getAllFileList(file);
+        for (File file1 : allFileList) {
+            if(file1.isDirectory()){
+                res.add(file1);
+            }
+        }
+        return res;
+    }
+    public static List<File> getFileList(File file){
+        ArrayList<File> res = new ArrayList<>();
+        File[] allFileList = getAllFileList(file);
+        for (File file1 : allFileList) {
+            if(file1.isFile()){
+                res.add(file1);
+            }
+        }
+        return res;
+    }
+    public static File[] getAllFileList(File file){
+        if(file.exists() && file.isDirectory()){
+            return file.listFiles();
+        }
+        return null;
     }
 }
