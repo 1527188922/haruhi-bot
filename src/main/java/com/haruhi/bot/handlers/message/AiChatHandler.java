c80aa2b8b7da34c46a0dec3742c8423ea552c7aa
刘昉 <1527188922@qq.com>
2022-08-19 09:15:01 +0800

    ai聊天修复bug群仅at时错误回复

--- src/main/java/com/haruhi/bot/handlers/message/AiChatHandler.java
+++ src/main/java/com/haruhi/bot/handlers/message/AiChatHandler.java
@@ -14,6 +14,7 @@ import com.haruhi.bot.utils.RestUtil;
 import com.haruhi.bot.ws.Client;
 import com.simplerobot.modules.utils.KQCodeUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
@@ -78,6 +79,9 @@ public class AiChatHandler implements IMessageEvent {
                 for (String cq : this.cqs) {
                     s = s.replace(cq,"");
                 }
+            }
+            if(Strings.isBlank(s)){
+                return;
             }
 
             HashMap<String, Object> urlParam = new HashMap<>();
