0734e223db70fec7b00155f307111d2566dbb4b4
haruhi <1527188922@qq.com>
2022-08-28 12:05:34 +0800

    linux启动脚本优化

--- src/main/resources/build/script/startup.sh
+++ src/main/resources/build/script/startup.sh
@@ -1,3 +1,3 @@
 path=`pwd`
-nohup java -jar -Xms512m -Xmx512m $path/haruhiBot-server.jar > $path/haruhiBot.log 2>&1 &
+>$path/haruhiBot.log && nohup java -jar -Xms512m -Xmx512m $path/haruhiBot-server.jar > $path/haruhiBot.log 2>&1 &
 tail -f haruhiBot.log
