b5e9c417e30dd99b317a9c9e0c1fe7924641cdf4
haruhi <1527188922@qq.com>
2022-09-10 09:24:08 +0800

    订阅新闻开发part_2

--- src/main/java/com/haruhi/bot/handlers/message/news/SubscribeNewsHandler.java
+++ src/main/java/com/haruhi/bot/handlers/message/news/SubscribeNewsHandler.java
@@ -1,14 +1,20 @@
 package com.haruhi.bot.handlers.message.news;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.haruhi.bot.constant.GocqActionEnum;
 import com.haruhi.bot.constant.RegexEnum;
 import com.haruhi.bot.constant.event.MessageEventEnum;
 import com.haruhi.bot.dto.gocq.response.Message;
+import com.haruhi.bot.entity.SubscribeNews;
 import com.haruhi.bot.event.message.IMessageEvent;
 import com.haruhi.bot.factory.ThreadPoolFactory;
 import com.haruhi.bot.service.news.SubscribeNewsService;
+import com.haruhi.bot.ws.Client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -32,13 +38,40 @@ public class SubscribeNewsHandler implements IMessageEvent {
             return false;
         }
         ThreadPoolFactory.getCommandHandlerThreadPool().execute(()->{
+            try {
+                LambdaQueryWrapper<SubscribeNews> queryWrapper = new LambdaQueryWrapper<>();
+                boolean isGroup = false;
+                if (MessageEventEnum.group.getType().equals(message.getMessage_type())) {
+                    isGroup = true;
+                    queryWrapper.eq(SubscribeNews::getGroupId,message.getGroup_id()).eq(SubscribeNews::getType,1);
+                }else if (MessageEventEnum.privat.getType().equals(message.getMessage_type())){
+                    queryWrapper.eq(SubscribeNews::getSubscriber,message.getUser_id()).eq(SubscribeNews::getType,2);
+                }
+                int count = subscribeNewsService.count(queryWrapper);
+                if(count > 0){
+                    String tip = "";
+                    if(isGroup){
+                        tip = "本群已订阅新闻";
+                    }else{
+                        tip = "你已订阅新闻";
+                    }
+                    Client.sendMessage(message.getUser_id(),message.getGroup_id(),message.getMessage_type(),tip, GocqActionEnum.SEND_MSG,true);
+                    return;
+                }
 
-            int type = 1;
-            Integer count = 0;
-            if (MessageEventEnum.group.getType().equals(message.getMessage_type())) {
-
-            }else if (MessageEventEnum.privat.getType().equals(message.getMessage_type())){
-                type = 2;
+                SubscribeNews param = new SubscribeNews();
+                param.setSubscriber(message.getUser_id());
+                param.setCreateTime(new Date());
+                if(isGroup){
+                    param.setGroupId(message.getGroup_id());
+                    param.setType(1);
+                }else{
+                    param.setType(2);
+                }
+                subscribeNewsService.save(param);
+                Client.sendMessage(message.getUser_id(),message.getGroup_id(),message.getMessage_type(),"订阅成功", GocqActionEnum.SEND_MSG,true);
+            }catch (Exception e){
+                log.info("订阅新闻异常",e);
             }
         });
 
