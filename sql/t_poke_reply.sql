15975086b7a78a0a123ae31816c58af84887956b
刘昉 <1527188922@qq.com>
2022-09-22 10:48:57 +0800

    添加sql文件

---
+++ sql/t_poke_reply.sql
@@ -0,0 +1,17 @@
+INSERT INTO `t_poke_reply` VALUES (1, 'lsp你再戳？');
+INSERT INTO `t_poke_reply` VALUES (2, '连个可爱美少女都要戳的肥宅真恶心啊。');
+INSERT INTO `t_poke_reply` VALUES (3, '你再戳！');
+INSERT INTO `t_poke_reply` VALUES (4, '？再戳试试？');
+INSERT INTO `t_poke_reply` VALUES (5, '别戳了别戳了再戳就坏了555');
+INSERT INTO `t_poke_reply` VALUES (6, '我爪巴爪巴，球球别再戳了');
+INSERT INTO `t_poke_reply` VALUES (7, '你戳你🐎呢？！');
+INSERT INTO `t_poke_reply` VALUES (8, '那...那里...那里不能戳...绝对...');
+INSERT INTO `t_poke_reply` VALUES (9, '(。´・ω・)ん?');
+INSERT INTO `t_poke_reply` VALUES (10, '有事恁叫我，别天天一个劲戳戳戳！');
+INSERT INTO `t_poke_reply` VALUES (11, '欸很烦欸！你戳🔨呢');
+INSERT INTO `t_poke_reply` VALUES (12, '?');
+INSERT INTO `t_poke_reply` VALUES (13, '再戳一下试试？');
+INSERT INTO `t_poke_reply` VALUES (14, '???');
+INSERT INTO `t_poke_reply` VALUES (15, '正在关闭对您的所有服务...关闭成功');
+INSERT INTO `t_poke_reply` VALUES (16, '啊呜，太舒服刚刚竟然睡着了。什么事？');
+INSERT INTO `t_poke_reply` VALUES (17, '正在定位您的真实地址...定位成功。轰炸机已起飞');
