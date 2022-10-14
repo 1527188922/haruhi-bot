456e15026e186524d94b8777623ecaae046978b3
刘昉 <1527188922@qq.com>
2022-10-14 15:51:06 +0800

    http同意响应对象

--- src/main/java/com/haruhi/bot/controller/SystemController.java
+++ src/main/java/com/haruhi/bot/controller/SystemController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.haruhi.bot.config.BotConfig;
 import com.haruhi.bot.config.path.AbstractPathConfig;
+import com.haruhi.bot.dto.http.HttpResponse;
 import com.haruhi.bot.utils.system.SystemInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,12 @@ public class SystemController {
     private AbstractPathConfig abstractPathConfig;
 
     @RequestMapping("/status")
-    public Map status(){
+    public HttpResponse<Map> status(){
         Map<String, JSONObject> resp = new HashMap<>(3);
         resp.put(BotConfig.class.getSimpleName(), JSON.parseObject(BotConfig.toJson()));
         resp.put(SystemInfo.class.getSimpleName(),JSON.parseObject(SystemInfo.toJson()));
         resp.put(AbstractPathConfig.class.getSimpleName(),JSON.parseObject(abstractPathConfig.toString()));
 
-        return resp;
+        return HttpResponse.success(resp);
     }
 }
